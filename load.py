f38c0fbf498c034feb4d31e28e86557fa6d271c1
Juan D. Bravo <196306536+juan-dbravo@users.noreply.github.com>
2025-06-20 14:44:45 +0000

    feat: refactor scripts to use structured folders (raw, clean, transformed, final)

--- load.py
+++ load.py
@@ -1,2 +1,14 @@
-print("✅ Data loading simulated.")
+import os
 
+# Ensure the final otput directory exists
+os.makedirs("data/final", exist_ok=True)
+
+# Simulate reading transformed data
+with open("data/transformed/data_transformed.csv", "r") as infile:
+    lines = infile.readlines()
+
+# Simulate writing final loaded data
+with open("data/final/final_output.csv", "w") as outfile:
+    outfile.writelines(lines)
+
+print("✅ Final output saved to data/final/final_output.csv")
